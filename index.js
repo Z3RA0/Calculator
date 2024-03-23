ae2000e8627e89ef347977fda25c78c38249ccc7
Z3RA0 <37659150+Z3RA0@users.noreply.github.com>
2024-03-23 15:41:14 -0400

    everything work

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let firstNum = '';
 let secondNum = '';
 let operatorSelected = '';
+let total = '';
 
 // functions for numbers
 const resultScreen = document.querySelector('.resultScreen');
@@ -9,28 +10,65 @@ const currentInput = document.querySelector('.currentInput')
 const fullEquation = document.querySelector('.fullEquation')
 
 function getFirstNum(btn) {
-  currentInput.innerText += btn.innerText;
+  currentInput.textContent += btn;
   resultScreen.append(currentInput);
-  firstNum = parseFloat(currentInput.innerText);
+  firstNum = parseFloat(currentInput.textContent);
   fullEquation.textContent = firstNum;
   resultScreen.prepend(fullEquation);
 }
 
 function getOperator(btn) {
-  currentInput.innerText = '';
-  currentInput.innerText += btn.innerText;
-  resultScreen.append(currentInput);
-  operatorSelected = btn.innerText;
-  fullEquation.textContent = firstNum + ' ' + operatorSelected;
-  resultScreen.prepend(fullEquation);
+  if (operatorSelected === '') {
+    currentInput.textContent = '';
+    currentInput.textContent = btn;
+    resultScreen.append(currentInput);
+    operatorSelected = btn;
+    fullEquation.textContent = firstNum + ' ' + operatorSelected;
+    resultScreen.prepend(fullEquation);
+  } else {
+    let total = Math.round(operate(firstNum, secondNum) * 100)/100;
+    firstNum = total;
+    secondNum = '';
+    currentInput.textContent = btn;
+    fullEquation.textContent = firstNum + ' ' + btn;
+    resultScreen.prepend(fullEquation);
+    resultScreen.append(currentInput);
+  }
 }
 
 function getSecondNum(btn) {
-  currentInput.innerText += btn.innerText;
+  currentInput.textContent += btn;
   resultScreen.append(currentInput);
-  secondNum = parseFloat(currentInput.innerText);
+  secondNum = parseFloat(currentInput.textContent);
   fullEquation.textContent = firstNum + ' ' + operatorSelected + ' ' + secondNum;
   resultScreen.prepend(fullEquation);
+}
+
+function getEqual(btn) {
+  if (operatorSelected === '÷' && secondNum === 0) {
+    currentInput.textContent = 'cannot divide by 0 buddy';
+    resultScreen.append(currentInput);
+    secondNum = ''
+  } else if (firstNum !== '' && operatorSelected !== '' && secondNum !== '') {
+  total = Math.round(operate(firstNum, secondNum) * 100)/100;
+  currentInput.textContent = total;
+  resultScreen.prepend(fullEquation);
+  resultScreen.append(currentInput);
+  firstNum = total;
+  secondNum = '';
+  operatorSelected = '';
+  }
+}
+
+function getRefresh() {
+  total = '';
+  firstNum = '';
+  secondNum = '';
+  operatorSelected = '';
+  fullEquation.textContent = '';
+  currentInput.textContent = '';
+  resultScreen.prepend(fullEquation);
+  resultScreen.append(currentInput);
 }
 
 // function for operators
@@ -52,7 +90,7 @@ function operate(a, b) {
     return add(a, b)
   } else if (operatorSelected === '-'){
     return subtract(a, b)
-  } else if (operatorSelected ==='X'){
+  } else if (operatorSelected ==='x'){
     return multiply(a, b)
   } else if (operatorSelected === '÷'){
     return divide(a, b)
@@ -61,74 +99,128 @@ function operate(a, b) {
 
 // event listeners
 const numbers = document.querySelectorAll('.numBtn');
+
+numbers.forEach(number => {
+  number.addEventListener('click', () => {
+    if (currentInput.textContent.length < 15) {
+      if (total !== '') {
+        getRefresh();
+        num = number.textContent;
+        getFirstNum(num);
+      } else if (operatorSelected === '') {
+        num = number.textContent;
+        getFirstNum(num);
+      } else if (operatorSelected !== '' && secondNum === ''){
+        num = number.textContent;
+        currentInput.textContent = '';
+        getSecondNum(num);
+      } else {
+        num = number.textContent;
+        getSecondNum(num);
+      }
+  }
+});
+});
+
 const operators = document.querySelectorAll('.operatorBtn');
 
-const decimal = document.querySelector('#decimal');
+operators.forEach(operator => {
+  operator.addEventListener('click', () => {
+    oper = operator.textContent;
+    getOperator(oper);
+    }
+  )}
+);
+
 const back = document.querySelector('#back');
 
 back.addEventListener('click', () => {
   if (secondNum === '') {
     currentInput.textContent = currentInput.textContent.slice(0, -1);
-    firstNum = parseFloat(currentInput.innerText);
+    firstNum = parseFloat(currentInput.textContent);
     fullEquation.textContent = firstNum;
   } else {
     currentInput.textContent = currentInput.textContent.slice(0, -1);
-    secondNum = parseFloat(currentInput.innerText);
+    secondNum = parseFloat(currentInput.textContent);
     fullEquation.textContent = firstNum + ' ' + operatorSelected + ' ' + secondNum;
   }
 });
 
+const decimal = document.querySelector('#decimal');
+
 decimal.addEventListener('click', () => {
   if (currentInput.textContent.split('').includes('.') === false) {
-    currentInput.innerText += '.';
+    currentInput.textContent += '.';
     resultScreen.append(currentInput);
   }
-})
-
-operators.forEach(operator => {
-  operator.addEventListener('click', () => getOperator(operator));
-});
-
-numbers.forEach(number => {
-    number.addEventListener('click', () => {
-      if (currentInput.textContent.length < 15) {
-        if (operatorSelected === '') {
-          getFirstNum(number);
-        } else if (operatorSelected !== '' && secondNum === ''){
-          currentInput.innerText = '';
-          getSecondNum(number);
-        } else {
-          getSecondNum(number);
-        }
-    }
-  });
 });
 
 const equalBtn = document.querySelector('#equal');
-const refreshBtn = document.querySelector('#refresh');
 
 equalBtn.addEventListener('click', () => {
-  if (operatorSelected === '÷' && secondNum === 0) {
-    currentInput.textContent = 'cannot divide by 0 buddy';
-    resultScreen.append(currentInput);
-    secondNum = ''
-  } else if (firstNum !== '' && operatorSelected !== '' && secondNum !== '') {
-  let total = Math.round(operate(firstNum, secondNum) * 100)/100;
-  fullEquation.textContent = '';
-  currentInput.textContent = total;
-  resultScreen.prepend(fullEquation);
-  resultScreen.append(currentInput);
-  firstNum = total;
-  secondNum = '';
+  getEqual();
+});
+
+const refreshBtn = document.querySelector('#refresh');
+
+refreshBtn.addEventListener('click', () => {
+  getRefresh();
+});
+
+// keyboard support
+
+document.addEventListener('keydown', (e) => {
+  if (!isNaN(e.key)) {
+    if (currentInput.textContent.length < 15) {
+      if (operatorSelected === '') {
+        getFirstNum(e.key);
+      } else if (operatorSelected !== '' && secondNum === ''){
+        currentInput.textContent = '';
+        getSecondNum(e.key);
+      } else {
+        getSecondNum(e.key);
+      }
+    }
   }
 });
 
-refreshBtn.addEventListener('click', () => {
-  total = '';
-  firstNum = '';
-  secondNum = '';
-  operatorSelected = '';
-  fullEquation.textContent = '';
-  currentInput.textContent = '';
+document.addEventListener('keydown', (e) => {
+  if (e.key === '+' || e.key === '-' || e.key === 'x') {
+    getOperator(e.key);
+  } else if (e.key === '/') {
+    e.divide = '÷';
+    getOperator(e.divide);
+  }
 });
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === '.') {
+    if (currentInput.textContent.split('').includes('.') === false) {
+      currentInput.textContent += '.';
+      resultScreen.append(currentInput);
+    }
+  }
+});
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === '=' || e.key === 'Enter') {
+    getEqual();
+  }
+});
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Backspace') {
+    if (secondNum === '') {
+      currentInput.textContent = currentInput.textContent.slice(0, -1);
+      firstNum = parseFloat(currentInput.textContent);
+      fullEquation.textContent = firstNum;
+    } else {
+      currentInput.textContent = currentInput.textContent.slice(0, -1);
+      secondNum = parseFloat(currentInput.textContent);
+      fullEquation.textContent = firstNum + ' ' + operatorSelected + ' ' + secondNum;
+    }
+  }
+});
+
+
 
