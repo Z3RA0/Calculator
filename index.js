024f212531c90183874dcff4b694292457f5aa82
Z3RA0 <37659150+Z3RA0@users.noreply.github.com>
2024-03-23 00:49:52 -0400

    number & operator input displays> + firstNum, operator and secondNum values are stored

--- index.js
+++ index.js
@@ -1,33 +1,85 @@
 // assign input to variables
-let firstNum;
-let secondNum;
-let operator;
+let firstNum = '';
+let secondNum = '';
+let operatorSelected = '';
+
+
+// Display input on screen
+const numbers = document.querySelectorAll('.numBtn');
+const operators = document.querySelectorAll('.operatorBtn');
+
+const resultScreen = document.querySelector('.resultScreen');
+const currentInput = document.querySelector('.currentInput')
+const fullEquation = document.querySelector('.fullEquation')
+
+function firstNumInput(btn) {
+  currentInput.innerText += btn.innerText;
+  resultScreen.append(currentInput);
+  firstNum = parseFloat(currentInput.innerText);
+  fullEquation.textContent = firstNum;
+  resultScreen.prepend(fullEquation);
+}
+
+function operatorInput(btn) {
+  currentInput.innerText = '';
+  currentInput.innerText += btn.innerText;
+  resultScreen.append(currentInput);
+  operatorSelected = btn.innerText;
+  console.log(operatorSelected);
+  fullEquation.textContent = firstNum + operatorSelected;
+  resultScreen.prepend(fullEquation);
+}
+
+function secondNumInput(btn) {
+  currentInput.innerText += btn.innerText;
+  resultScreen.append(currentInput);
+  secondNum = parseFloat(currentInput.innerText);
+  fullEquation.textContent = firstNum + operatorSelected + secondNum;
+  resultScreen.prepend(fullEquation);
+}
+
+operators.forEach(operator => {
+  operator.addEventListener('click', () => operatorInput(operator));
+});
+
+
+numbers.forEach(number => {
+  number.addEventListener('click', () => {
+    if (operatorSelected === '') {
+      firstNumInput(number);
+    } else if (operatorSelected !== '' && secondNum === ''){
+      currentInput.innerText = '';
+      secondNumInput(number);
+    } else {
+      secondNumInput(number);
+    }
+  });
+});
 
 // function for operator
-function add(firstNum, secondNum) {
-  return firstNum + secondNum;
+function add(a, b) {
+  return a + b;
 };
-function subtract(firstNum, secondNum) {
-  return firstNum - secondNum;
+function subtract(a, b) {
+  return a - b;
 };
-function multiply(firstNum, secondNum) {
-  return firstNum * secondNum;
+function multiply(a, b) {
+  return a * b;
 };
-function divide(firstNum, secondNum) {
-  return firstNum / secondNum;
+function divide(a, b) {
+  return a / b;
 };
 
-function operate(operator) {
+function operate(a, b) {
   if (operator === '+') {
-    return add(firstNum, secondNum)
+    return add(a, b)
   } else if (operator === '-'){
-    return subtract(firstNum, secondNum)
-  } else if (operator ==='*'){
-    return multiply(firstNum, secondNum)
-  } else if (operator === '/'){
-    return divide(firstNum, secondNum)
+    return subtract(a, b)
+  } else if (operator ==='X'){
+    return multiply(a, b)
+  } else if (operator === '+'){
+    return divide(a, b)
   } else {
     return alert('invalid input')
   }
 }
-
